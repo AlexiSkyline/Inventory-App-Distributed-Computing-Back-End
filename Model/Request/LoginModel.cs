a94996153af1e2f68ce14f89d97d569862e40212
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-23 23:44:52 -0600

    We create SingleResponse, LoginModel and LoginResponse Models for the Login Route

---
+++ Model/Request/LoginModel.cs
@@ -0,0 +1,6 @@
+namespace Unach.Inventory.API.Model.Request {
+    public class LoginModel {
+        public string? UserName { get; set; }
+        public string? Password{ get; set; }
+    }
+}
---
+++ Model/SingleResponse.cs
@@ -0,0 +1,6 @@
+namespace Unach.Inventory.API.Model {
+    public class SingleResponse {
+        public bool? SuccessStatus { get; set; }
+        public string? Message { get; set; }
+    }
+}
