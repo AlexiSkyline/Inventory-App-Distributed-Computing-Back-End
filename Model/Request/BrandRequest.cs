211d27b7a4a600687c4d7a849c43bac20deafabd
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-26 20:56:54 -0600

    We Create BrandResponse and BrandRequest Models

---
+++ Model/Request/BrandRequest.cs
@@ -0,0 +1,6 @@
+namespace Unach.Inventory.API.Model.Request;
+
+public class BrandRequest {
+    public int? Id { get; set; }
+    public int? Description { get; set; }
+}
---
+++ Model/Response/BrandResponse.cs
@@ -0,0 +1,6 @@
+namespace Unach.Inventory.API.Model.Response;
+
+public class BrandResponse {
+    public int? Id { get; set; }
+    public int? Description { get; set; }
+}
